6d4141c41e66cbab7b5ecfafe700721d5f266cde
ridzuan <ridz0n391@gmail.com>
2017-02-16 14:57:21 +0800

    update

---
+++ GitProject/TestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TestViewController.swift
+//  GitProject
+//
+//  Created by MacBook 1 on 16/02/2017.
+//  Copyright © 2017 MacBook 1. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
